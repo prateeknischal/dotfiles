48c363b26865c07f22eb7683b5e4c3cc8025ba9c
Prateek Kumar Nischal <prateeknischal25@gmail.com>
2024-01-18 01:30:49 -0800

    Try with new autocomplete behavior

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -673,20 +673,18 @@ cmp.setup {
     completeopt = 'menuone,noinsert',
   },
   mapping = cmp.mapping.preset.insert {
-    ['<CR>'] = cmp.mapping.select_next_item(),
+    --['<CR>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<Tab>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
+      --behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
     ['<CR>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -694,8 +692,6 @@ cmp.setup {
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
