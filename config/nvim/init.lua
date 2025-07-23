647f892d2d7c1234eee02ac39f425385e413c576
Prateek Kumar Nischal <prateeknischal25@gmail.com>
2025-07-23 13:51:33 -0700

    Remove mason-lspconfig for duplicate lsp
    Remove mason-lspconfig since it spawns the default arg lsp server along
    side the custom config lsp server later in the init.lua.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -79,14 +79,7 @@ require('lazy').setup({
 
   -- Comment like a pro
   'scrooloose/nerdcommenter',
-  {
-    'mason-org/mason-lspconfig.nvim',
-    opts = {},
-    dependencies = {
-      { 'mason-org/mason.nvim', opts = {} },
-      'neovim/nvim-lspconfig'
-    }
-  },
+  'mason-org/mason.nvim',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
@@ -571,50 +564,26 @@ end, 0)
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(client, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
+  --vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go To Definition" })
+  local builtins = require('telescope.builtin');
+  vim.keymap.set('n', 'gd', builtins.lsp_definitions, { desc = "Go To Definition" })
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "Find references" })
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Find declaration" })
+  vim.keymap.set('n', 'gc', vim.lsp.buf.incoming_calls, { desc = "Get incoming calls" })
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('gc', vim.lsp.buf.incoming_calls, '[G]oto [C]allers')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>f', vim.lsp.buf.format, '[F]ormat [B]uffer')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Hover" })
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Find Implementation" })
+  vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = "Format Buffer" })
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" })
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder" })
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename" })
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Action" })
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
-
-  nmap('<leader>f', vim.lsp.buf.format, '[F]ormat [B]uffer')
 
   client.server_capabilities.semanticTokensProvider = nil
 end
@@ -640,33 +609,6 @@ require('which-key').add({
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
-require('mason-lspconfig').setup()
-
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
-local servers = {
-  clangd = {},
-  ansiblels = {},
-  bashls = {},
-  rust_analyzer = {},
-  jsonls = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
-    },
-  },
-}
 
 -- Setup neovim lua configuration
 require('neodev').setup()
@@ -675,25 +617,7 @@ require('neodev').setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-require('mason-lspconfig').setup {
-  automatic_enable = false,
-  ensure_installed = vim.tbl_keys(servers),
-  setup_handlers = function(server_name)
-    if server_name ~= 'jdtls' then
-      -- tell mason to not handle jdtls init for me
-      -- since I have a custom config for it in ftplugin
-      require('lspconfig')[server_name].setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = servers[server_name],
-        filetypes = (servers[server_name] or {}).filetypes,
-      }
-    end
-  end,
-}
-
 -- [[ Configure litee.calltree ]]
-
 require('litee.lib').setup({
   panel = {
     orientation = "right",
@@ -891,7 +815,6 @@ lspconfig.gopls.setup({
         rangeVariableTypes = true,
       },
       analyses = {
-        fieldalignment = true,
         nilness = true,
         unusedparams = true,
         unusedwrite = true,
